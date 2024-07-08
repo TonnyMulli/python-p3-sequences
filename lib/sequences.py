43cb4d97cf84e12ad41250d46e487594581e0345
Tonny Mulli <tonnymulli2118@gmail.com>
2024-07-08 22:18:44 +0300

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]
+    if length == 0:
+        fib_sequence = []
+    elif length == 1:
+        fib_sequence = [0]
+    while len(fib_sequence) < length:
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+    print(fib_sequence)
+
+    
+
+    
